67053030282241c3ede16577b0566063b4ab4938
Jagrut Chaudhari <jagrutchaudhari18@gmail.com>
2020-07-24 15:54:09 +0530

    added topic container

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <template>
   <div id="app">
+    <div id="header_bar">
+      <p id="header">Essayister</p>
+    </div>
+    <Topic topicText="To understand the most important characteristics of a society, one must study its major cities. Write a response in which you discuss the extent to which you agree or disagree with the statement and explain your reasoning for the position you take. In developing and supporting your position, you should consider ways in which the statement might or might not hold true and explain how these considerations shape your position." />
     <img alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vueebbbbaaae.js App" />
   </div>
@@ -7,22 +11,41 @@
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
+import Topic from "./components/Topic.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld
+    HelloWorld,
+    Topic
   }
 };
 </script>
 
 <style>
+@import url("https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap");
+
+* {
+  font-family: "Montserrat", sans-serif;
+}
+
+body, html {
+  background-color: #F4FFF7;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+#header {
+  font-size: 20;
+  font-weight: bold;
+  color: #007969;
+}
+#header_bar {
+  display: flex;
+  flex-direction: row;
+  justify-content: left;
 }
 </style>
---
+++ src/components/Topic.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="topic_container">
+    <h3>{{topicText}}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Topic",
+  props: {
+    topicText: String
+  }
+};
+</script>
+
+<style scoped>
+#topic_container {
+  border: 0.5px solid #007969;
+  border-radius: 7px;
+  padding: 10px;
+  margin: 0px 40px;
+}
+h3{
+  font-size: 16px;
+  font-weight: 400;
+}
+</style>
